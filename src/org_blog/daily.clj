f91c9316d90b98cb7b0a5c81d51a16a3c94b7de6
Russell Matney <russell.matney@gmail.com>
2022-10-05 16:43:39 -0400

    feat: daily clean up - remove no-tag 'nil's, unnecessary filter
    org-crud now supports filtering out :private: tagged items/files by
    default, so no need to do it in here.
    
    Removes the TOC, which doesn't work for dynamic markdown content.
    
    Refactors the impl to avoid printing 'nil' when a tag-group has no items
    for a particular day.

--- src/org_blog/daily.clj
+++ src/org_blog/daily.clj
@@ -1,69 +1,34 @@
 (ns org-blog.daily
-  {:nextjournal.clerk/visibility {:code :hide :result :hide}
-   :nextjournal.clerk/toc        true}
+  {:nextjournal.clerk/visibility {:code :hide :result :hide}}
   (:require
    [clojure.string :as string]
    [clojure.set :as set]
-   [clojure.walk :as walk]
    [garden.core :as garden]
    [org-crud.core :as org-crud]
    [org-crud.markdown :as org-crud.markdown]
    [nextjournal.clerk :as clerk]
    [tick.core :as t]
-
    [org-blog.render :as render]))
 
 (defonce ^:dynamic day (t/today))
+
+(def this-ns *ns*)
 
 (defn export-for-day [{:keys [day]}]
   (with-bindings {#'org-blog.daily/day day}
     (render/path+ns-sym->spit-static-html
       (str "public/daily/" day ".html")
-      ;; TODO infer/un-hard-code this
-      'org-blog.daily)))
+      (symbol (str this-ns)))))
 
 (comment
   (export-for-day {:day (t/today)})
-  (export-for-day {:day (str (t/<< (t/today) (t/new-period 1 :days)))})
-
-  (with-bindings {#'org-blog.daily/day (t/yesterday)}
-    (->>
-      (render/eval-notebook 'org-blog.daily)
-      :blocks
-      (take 12)
-      last
-      :result
-      :nextjournal/value
-      :nextjournal/value)))
+  (export-for-day {:day (str (t/<< (t/today) (t/new-period 1 :days)))}))
 
 
 ^{::clerk/no-cache true}
 (def todays-org-item
-  (let [item (-> (garden/daily-path day) org-crud/path->nested-item)]
-    ;; recursively remove all :private: tagged items
-
-    ;; TODO pull this walk construct into a useful org-crud fn
-    (walk/postwalk
-      (fn [node]
-        (cond
-          (not (map? node)) node
-
-          (and
-            (map? node)
-            (seq (:org/items node)))
-          (update node :org/items
-                  (fn [items]
-                    (->>
-                      items
-                      (remove
-                        (fn [{:keys [org/tags]}]
-                          (seq (set/intersection
-                                 ;; tags that signal item rejection
-                                 #{"private"}
-                                 tags)))))))
-
-          :else node))
-      item)))
+  (-> (garden/daily-path day)
+      org-crud/path->nested-item))
 
 (def daily-items
   (->> todays-org-item :org/items))
@@ -74,36 +39,47 @@
        (filter (comp seq #(set/intersection tags %) :org/tags))))
 
 (comment
-  (items-with-tags #{"til"})
+  (->>
+    (items-with-tags #{"til"})
+    (mapcat org-crud.markdown/item->md-body))
   (items-with-tags #{"bugstory"}))
 
-(defn md-for-items-with-tags
+(defn content-with-tags
   [{:keys [title tags]}]
   (let [notes
         (->>
           (items-with-tags tags)
-          (mapcat org-crud.markdown/item->md-body))]
-    ;; TODO don't print `nil` when there are none, you dingus
+          (mapcat (fn [item]
+                    (let [[title & body]
+                          (org-crud.markdown/item->md-body item)]
+                      (concat
+                        [title
+                         (->> (:org/tags item)
+                              (string/join ":")
+                              (#(str "tags: :" % ":")))]
+                        body))))
+          seq)]
     (when (seq notes)
-      (clerk/md
-        (str
-          "## " title "\n"
-          (->> notes (string/join "\n")))))))
+      (concat
+        [(str "## " title "\n")]
+        notes))))
+
+(comment
+  (content-with-tags
+    {:title "TIL" :tags #{"til"}}))
+
+(def tag-groups
+  [{:title "TIL" :tags #{"til"}}
+   {:title "Stories" :tags #{"bugstory"}}
+   {:title "Hammocking" :tags #{"hammock"}}])
+
+(defn content-for-tag-groups []
+  (->> tag-groups
+       (mapcat content-with-tags)
+       (string/join "\n")))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 {::clerk/visibility {:result :show}}
 
-(clerk/md
-  (str "# " (:org/name todays-org-item)))
-
-(md-for-items-with-tags
-  {:title "TIL"
-   :tags  #{"til"}})
-
-(md-for-items-with-tags
-  {:title "Bug Stories"
-   :tags  #{"bugstory"}})
-
-(md-for-items-with-tags
-  {:title "Hammock"
-   :tags  #{"hammock"}})
+(clerk/md (str "# " (:org/name todays-org-item)))
+(clerk/md (content-for-tag-groups))
