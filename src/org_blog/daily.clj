4b35ff875ece1ee3222e7687ff22bdd245229999
Russell Matney <russell.matney@gmail.com>
2022-10-05 20:56:05 -0400

    feat: clj-kondo rules, reload button on dailies
    Also starting the clerk server in user.clj.

--- src/org_blog/daily.clj
+++ src/org_blog/daily.clj
@@ -14,6 +14,7 @@
 (defonce ^:dynamic day (t/today))
 
 (def this-ns *ns*)
+(def this-file *file*)
 (declare todays-org-path)
 
 (defn export-for-day [{:keys [day]}]
@@ -26,6 +27,12 @@
             (str "public/daily/" day ".html")
             (symbol (str this-ns)))
           (println "[WARN] no daily file for " day " at path " path))))))
+
+(defn reexport! []
+  (export-for-day {:day day}))
+
+(defn reload! []
+  (clerk/show! this-file))
 
 (comment
   (export-for-day {:day (t/today)})
@@ -89,8 +96,26 @@
        (mapcat #(content-with-tags items %))
        (string/join "\n")))
 
+
+;; TODO flag for filtering elems when printing to 'public'
+
+^::clerk/no-cache
+(def last-load-time (atom (t/now)))
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 {::clerk/visibility {:result :show}}
+
+^{::clerk/viewer
+  '(fn [last-load-time]
+     (v/html [:div.text-center
+              (when last-load-time
+                [:div.mt-2.text-white.text-lg
+                 last-load-time])
+              [:button.bg-blue-500.hover:bg-blue-700.text-white.font-bold.py-2.px-4.rounded
+               {:on-click (fn [e] (v/clerk-eval '(reload!)))} "Reload 🎲!"]
+              [:button.bg-blue-500.hover:bg-blue-700.text-white.font-bold.py-2.px-4.rounded
+               {:on-click (fn [e] (v/clerk-eval '(re-export!)))} "Re-export 🎲!"]]))}
+(str @last-load-time)
 
 (clerk/md (str "# " (:org/name todays-org-item)))
 (clerk/md (content-for-tag-groups (:org/items todays-org-item)))
--- src/user.clj
+++ src/user.clj
@@ -1,8 +1,18 @@
 (ns user
   (:require
    [wing.repl :as repl]
-   [nextjournal.clerk :as clerk]))
+   [nextjournal.clerk :as clerk]
+   [babashka.fs :as fs]
+   [clojure.string :as string]))
+
+;; go ahead and start this whenever the repl starts up
+(clerk/serve! {:port 8888})
+
 
 (comment
-  (repl/sync-libs!)
-  (clerk/serve! {:port 8888}))
+  (clerk/show!
+    (-> *file*
+        (string/replace (fs/file-name *file*) "org_blog/daily.clj")
+        (doto (println "is clerk-show!ing"))))
+
+  (repl/sync-libs!))
