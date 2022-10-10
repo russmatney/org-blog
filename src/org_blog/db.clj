5ffc526dc60a74204714e1bfa52271b8bfc4f860
Russell Matney <russell.matney@gmail.com>
2022-10-10 15:59:26 -0400

    feat: linked-notes list with publish/unpublish buttons
    Clicking these publish/unpublish adds/removes the note's id from the
    published-ids atom, which re-evals the export page, pulling in newly
    linked items. Pretty cool! Definitely going to grow unwieldy - should
    reach for an interactive graph soon.

--- src/org_blog/db.clj
+++ src/org_blog/db.clj
@@ -2,8 +2,10 @@
   {:nextjournal.clerk/visibility {:code :hide :result :hide}}
   (:require
    [util :refer [ensure-uuid]]
-   [garden.core :as garden]))
+   [garden.core :as garden]
+   [org-crud.core :as org-crud]))
 
+(println "parsing all notes")
 (def all-notes
   (garden/all-garden-notes-nested))
 
@@ -13,8 +15,28 @@
     (map (fn [n] [(:org/id n) n]))
     (into {})))
 
+(def root-note-by-child-id
+  (->>
+    notes-by-id
+    (mapcat (fn [[p-id note]]
+              (concat
+                [[p-id note]]
+                (->> note
+                     org-crud/nested-item->flattened-items
+                     (map :org/id)
+                     (remove nil?)
+                     (map (fn [c-id]
+                            [c-id p-id]))))))
+    (into {})))
+
+(defn root-note-for-any-id [id]
+  (let [note (root-note-by-child-id id)]
+    (if (map? note)
+      note
+      (notes-by-id note))))
+
 (defn fetch-with-id [id]
-  (notes-by-id (ensure-uuid id)))
+  (root-note-for-any-id (ensure-uuid id)))
 
 (comment
   (count all-notes)
--- src/org_blog/export.clj
+++ src/org_blog/export.clj
@@ -7,52 +7,50 @@
    [org-crud.core :as org-crud]
    [dates.tick :as dates.tick]
 
-   [org-blog.config :as config]
+   [util :as util]
    [org-blog.daily :as daily]
    [org-blog.note :as note]
-   [org-blog.db :as db]))
+   [org-blog.db :as db]
+   [ralphie.notify :as notify]))
 
 
 (defonce ^:dynamic
   ;; TODO improve how days are collected (maybe an 'earliest-date' makes sense)
   *days-ago* 7)
 (defonce ^:dynamic *days* (dates.tick/days *days-ago*))
-(defonce days-with-content (->> *days* (filter daily/items-for-day)))
-
 
 (defonce published-ids (atom #{}))
-(defonce missing-ids (atom #{}))
-(defonce missing-items (atom #{}))
-
+(comment (reset! published-ids #{}))
 (defonce linked-items (atom #{}))
-(defn reset-linked-items [] (reset! linked-items #{}))
 
 (defn collect-linked-ids
+  "Assumes published-ids is already set correctly"
   ([] (collect-linked-ids nil))
   ([opts]
    (let [days                  (:days opts *days*)
          published-daily-items (->> days (mapcat daily/items-for-day))
          published-notes       (->> @published-ids (map db/fetch-with-id))
          published             (concat published-daily-items published-notes)
-         all-items             (->> published
-                                    (mapcat org-crud/nested-item->flattened-items))
-         all-links             (->> all-items
-                                    (mapcat :org/links-to))]
+         all-items             (->> published (mapcat org-crud/nested-item->flattened-items))
+         all-links             (->> all-items (mapcat :org/links-to))]
      all-links)))
 
-(defn set-linked-items []
-  (let [linked-ids (->> (collect-linked-ids) (map :link/id) (into #{}))
-        missing    (set/difference linked-ids @published-ids)]
-    (reset! missing-ids missing)
-    (reset! missing-items (->> missing (map db/notes-by-id)))
-    (reset! linked-items (->> linked-ids (map db/notes-by-id)))))
+(defn reset-linked-items
+  "Resets the linked-items lists, which is made up of linked (i.e. published) and missing items."
+  []
+  (let [linked-ids (->> (collect-linked-ids) (map :link/id) (into #{}))]
+    (reset! linked-items (->> linked-ids (map db/fetch-with-id)))))
 
 (defn note->uri [item]
   ;; TODO dry up uri creation (maybe in config?)
   (-> item :org/source-file note/->uri))
 
+(defn days-with-prev+next
+  "Builds a list of `{:prev <date> :day <date> :next <date>}` day groups.
 
-(defn days-with-prev+next [days]
+  Useful for associating daily files so that prev/next daily links
+  jump to the next day with content, skipping empty days."
+  [days]
   (let [first-group [nil (first days) (second days)]
         groups
         (->>
@@ -84,11 +82,10 @@
         (if (@published-ids linked-id)
           ;; TODO to prefix or not to prefix - used as fs path or as href?
           (str "/" (note->uri item))
-          (do
-            (println "[INFO: missing link]: removing link to unpublished note: "
-                     (:org/name item))
-            (swap! missing-ids conj id)
-            nil))))))
+
+          ;; returning nil here to signal the link's removal
+          (println "[INFO: missing link]: removing link to unpublished note: "
+                   (:org/name item)))))))
 
 (defn publish-dailies
   ([] (publish-dailies nil))
@@ -114,7 +111,7 @@
 (comment
   (publish-notes))
 
-(set-linked-items)
+(reset-linked-items)
 
 (comment
   (def proc-gen-item
@@ -122,43 +119,82 @@
       @linked-items
       (filter (comp #(string/includes? % "proc") :org/name))
       first))
+  (swap! published-ids conj (:org/id proc-gen-item)))
 
-  (def level-design-item
-    (->>
-      @linked-items
-      (filter (comp #(string/includes? % "level design") string/lower-case :org/name))
-      first))
+(defn publish! [note-id]
+  (notify/notify "publishing note" note-id)
+  (swap! published-ids conj (util/ensure-uuid note-id)))
 
-  (def game-feel-item
-    (->>
-      @linked-items
-      (filter (comp #(string/includes? % "game feel") string/lower-case :org/name))
-      first))
+(defn unpublish! [note-id]
+  (notify/notify "unpublishing note" note-id)
+  (swap! published-ids disj (util/ensure-uuid note-id)))
 
-  (swap! published-ids conj (:org/id game-feel-item))
-  (swap! published-ids conj (:org/id proc-gen-item))
-  (swap! published-ids conj (:org/id level-design-item)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 {::clerk/visibility {:result :show}}
 
 ;; # export
 
+^{::clerk/no-cache true
+  ::clerk/viewer
+  {:name         :publishing-notes
+   :transform-fn clerk/mark-presented
+   :render-fn
+   '(fn [notes]
+      (v/html
+        [:div
+         (for [[i note] (->> notes (map-indexed vector))]
+           ^{:key i}
+           [:div
+            {:class ["flex" "flex-row" "space-x-4" "justify-between"]}
+
+            ;; label
+            [:div
+             (-> note :org/name)]
+
+            ;; actions
+            [:div
+             (when (not (:published note))
+               [:button
+                {:class    ["bg-green-700" "hover:bg-green-600"
+                            "text-slate-300" "font-bold"
+                            "py-2" "px-4" "m-1"
+                            "rounded"]
+                 :on-click (fn [_] (v/clerk-eval
+                                     `(org-blog.export/publish!
+                                        ~(-> note :org/id :nextjournal/value :value))))}
+                (str "publish: " (:org/name note))])
+
+             (when (:published note)
+               [:button
+                {:class    ["bg-blue-700" "hover:bg-blue-600"
+                            "text-slate-300" "font-bold"
+                            "py-2" "px-4" "m-1"
+                            "rounded"]
+                 :on-click (fn [_] (v/clerk-eval
+                                     `(org-blog.export/unpublish!
+                                        ~(-> note :org/id :nextjournal/value :value))))}
+                (str "unpublish: " (:org/name note))])]])]))}}
+(->> @linked-items
+     (map (fn [item] (assoc item :published (@published-ids (:org/id item)))))
+     (sort-by :published)
+     (into []))
+
+
+
 ;; ### published items
 (clerk/table
   {::clerk/width :full}
   (or
-    (->>
-      @published-ids
-      (map db/notes-by-id)
-      seq)
+    (->> @published-ids (map db/fetch-with-id) seq)
     [{:no-data nil}]))
 
 ;; ### missing items
 (clerk/table
   {::clerk/width :full}
   (or
-    (->> @missing-items seq)
+    (->> @linked-items (map :org/id) (into #{}) (#(set/difference % @published-ids))
+         (map db/fetch-with-id) seq)
     [{:no-data nil}]))
 
 ;; ### all linked items
