640b5cfd54ffb026891e9a87286b97da4628858a
Russell Matney <russell.matney@gmail.com>
2022-10-26 16:35:31 -0400

    feat: move index to last-modified, add notes, projects to new index page
    The index page is now the same, but preceeded by links to the about,
    other index pages, and whatever project pages have been published.

--- src/org_blog/pages/last_modified.clj
+++ src/org_blog/pages/last_modified.clj
@@ -1,4 +1,4 @@
-(ns org-blog.pages.index
+(ns org-blog.pages.last-modified
   {:nextjournal.clerk/visibility {:code :hide :result :hide}}
   (:require
    [nextjournal.clerk :as clerk]
@@ -6,9 +6,7 @@
    [dates.tick :as dates]
 
    [org-blog.item :as item]
-   [org-blog.notes :as notes]
-   [org-blog.uri :as uri]
-   [clojure.set :as set]))
+   [org-blog.notes :as notes]))
 
 (def ^:dynamic *notes* (notes/published-notes))
 
@@ -39,45 +37,11 @@
 (clerk/html
   [:div
    {:class ["flex" "flex-row" "justify-center"]}
-   [:h2 {:class ["font-mono"]} "Home"]])
-
-(clerk/html
-  [:div
-   [:h3 [:a {:href "/notes/blog_about.html"} "About"]]])
-
-(clerk/html
-  [:div
-   [:h3 "Notes"]
-   [:div
-    {:class "pl-4"}
-    [:h3
-     [:a {:href "/last-modified.html"} "...by Last Modified"
-      ]]
-    [:h3
-     [:a {:href "/tags.html"} "...by Tag"
-      ;; TODO include 5 most common tags
-      ]]]])
-
-(clerk/html
-  [:div
-   [:h3 "Projects"]
-   (->> (notes/published-notes)
-        (filter (comp seq
-                      #(set/intersection
-                         #{"project" "projects" "repo"} %)
-                      :org/tags))
-        (map (fn [note]
-               ;; TODO include link to repo
-               ;; TODO include short description
-               [:h3
-                [:a {:href (uri/note->uri note)}
-                 (:org/name note)]]))
-        (into [:div {:class "pl-4"}]))])
+   [:h2 {:class ["font-mono"]} "Notes By Last Modified"]])
 
 ^{::clerk/no-cache true}
 (clerk/html
   [:div
-   [:h3 "Recently modified"]
    [:div
     (->>
       (notes-by-day *notes*)
